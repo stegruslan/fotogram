1d9e999cd6bc2b70b6f2dd7995e811031f8b74bc
Руслан <ruslanstegancev175@gmail>
2024-04-29 13:06:10 +0300

    add image

--- src/app.py
+++ src/app.py
@@ -1,8 +1,16 @@
+"""Модуль для запуска FASTAPI."""
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def read_root() -> dict[str, str]:
+    """Тестовый эндпоинт.
+
+    Returns
+    -------
+        dict[str, str]: тестовый словарь.
+
+    """
     return {"Hello": "World"}
