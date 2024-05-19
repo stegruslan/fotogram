45a08fc2d7d32f5cc826795b44476eb7f6ae0bf7
Руслан <ruslanstegancev175@gmail>
2024-05-20 01:15:15 +0300

    add comments code

--- src/files/services.py
+++ src/files/services.py
@@ -7,15 +7,24 @@ from settings import settings
 
 
 def get_file(filename: str) -> FileResponse:
+    # Принимает имя файла и возвращает FileResponse
     with session_factory() as session:
+        # Создает сессию для взаимодействия с базой данных
         try:
             file_uuid, ext = filename.rsplit('.', 1)
+            # Разделяет имя файла на UUID и расширение
             file_model = session.query(FileModel).filter(
                 FileModel.uuid == file_uuid).first()
+            # Ищет файл в базе данных по UUID
             if not file_model or file_model.extension != ext:
+                # Проверяет, существует ли файл и совпадает ли расширение
                 raise HTTPException(status_code=404,
                                     detail=f'File {file_uuid} is not found')
+            # Если файл не найден или расширение не совпадает,
+            # выбрасывает исключение
             return FileResponse(path=settings.PATH_FILES / filename,
                                 filename=filename)
+        # Если файл найден и расширение совпадает, возвращает файл в ответе
         except ValueError as e:
             raise HTTPException(status_code=404, detail='Bad filename')
+            # Вызывает исключение HTTP 404, если формат имени файла неверный
--- src/posts/services.py
+++ src/posts/services.py
@@ -16,19 +16,39 @@ from database import session_factory
 async def create_post(current_user: CurrentUser,
                       content: Annotated[str, Form()],
                       files: list[UploadFile]) -> None:
+    # Определяет асинхронную функцию create_post,
+    # которая принимает текущего пользователя,
+    # содержимое поста и список файлов для загрузки, и возвращает None
     with session_factory() as session:
+        # Создается сессия для взаимодействия с базой данных
         post = Post(content=content, created_at=datetime.now(),
                     author=current_user)
+        # Создает объект поста с содержимым, текущей датой и автором
         session.add(post)
+        # Добавляет пост в сессию
         session.flush()
+        # Сбрасывает изменения, чтобы получить идентификатор поста
         for file in files:
+            # Итерирует по каждому файлу в списке файлов
             ext = file.filename.split('.')[-1]
+            # Извлекает расширение файла
             file_uuid = uuid.uuid4()
+            # Генерирует уникальный идентификатор для файла
             file_path = settings.PATH_FILES / (str(file_uuid) + "." + ext)
+            # Формирует путь к файлу,
+            # подставляя путь из настроек
+            # с уникальным идентификатором и расширением
             file_bytes = await file.read()
+            # Асинхронно читает содержимое файла
             with file_path.open(mode='wb') as f:
                 f.write(file_bytes)
+                # Открывает файл для записи в бинарном режиме
+                # и записывает содержимое
             file_model = FileModel(uuid=file_uuid, extension=ext,
                                    post_id=post.id)
+            # Создает объект FileModel с UUID, расширением
+            # и идентификатором поста
             session.add(file_model)
+            # Добавляет файл в сессию
         session.commit()
+        # Коммитит все изменения в базе данных
